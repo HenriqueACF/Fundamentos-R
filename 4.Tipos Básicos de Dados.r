58c0df472413fd9f24d23d677dbdd02a49dbb374
Henrique Assis <henrique_assis_10@hotmail.com>
2020-11-04 01:23:50 -0300

    tipos de dados

---
+++ 4.Tipos Básicos de Dados.r
@@ -0,0 +1,17 @@
+#Numeros
+#Inteiro - Integer
+4
+class(4L)
+# sempre adicionar o L para definir que é um numero inteiro
+
+#Ponto flutuante - numeric
+4.3
+class(4.3)
+
+#Caracteres - character
+"Henrique"
+class("Henrique")
+
+#Logico - Logico
+#TRUE or FALSE / T or F
+class(T)
