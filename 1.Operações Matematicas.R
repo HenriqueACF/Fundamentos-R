d7899fffd750bf53aad749cebae8c1d2cfebdcf2
Henrique Assis <henrique_assis_10@hotmail.com>
2020-11-04 00:56:28 -0300

    Operações basicas

---
+++ 1.Operações Matematicas.R
@@ -0,0 +1,26 @@
+#Comentario em R
+#Operações básicas
+
+#Adição
+4 + 5 
+
+#Subtração
+ 10 - 4
+
+#Mutiplicação
+4 * 5
+
+#Divisão
+20/5
+
+#Exponenciação
+4 ^ 2
+
+#Resto de divisão
+7 %% 2
+
+#Ordem de parenteses
+2 * (2 + 1)
+
+#Ordem matemática
+1 + 1 * 2
