1f021b42cee17c0550cfa15d2d42f68cb1746c8e
Henrique Assis <henrique_assis_10@hotmail.com>
2020-11-04 01:14:32 -0300

    up

---
+++ 3.Variaveis e verificação.r
@@ -0,0 +1,13 @@
+#Workspace básico
+#Criar variavel x
+x <- 0
+x
+#Criar mais uma variavel
+Teste <- "Henrique"
+Teste
+#Exibe as Variaveis declaradas no workspace
+ls()
+#Removendo variavel do Workspace
+rm(Nome)
+#Remover todas as Variaveis do Workspace
+rm(list = ls())
