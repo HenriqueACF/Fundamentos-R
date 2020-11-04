05e6d1cac2cfa543bfc777fac060acced4ff1e31
Henrique Assis <henrique_assis_10@hotmail.com>
2020-11-04 01:33:00 -0300

    verificando tipo de dados

---
+++ 5.Verificando tipo de dado.r
@@ -0,0 +1,13 @@
+#Verificação de tipo de dados
+var1 <- 5L
+var1
+class(var1)
+#Verifica se o tipo de dado é integer
+is.integer(var1)
+
+#Função is.Tipo(varial)
+is.integer
+is.numeric
+is.character
+is.double
+is.logical
