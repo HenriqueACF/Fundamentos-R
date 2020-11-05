ec4e6959473b32d194def0c99d0dfd1d90453790
Henrique Assis <henrique_assis_10@hotmail.com>
2020-11-05 00:07:17 -0300

    nomeando vetores

---
+++ 8.Nomeando Vetores.r
@@ -0,0 +1,14 @@
+#Nomeando vetores
+weekDays <- c("Dom" ,"Seg", "Ter", "Qua", "Qui", "Sex", "Sab")
+weekDays
+
+var <- c(1,2,3,4,5,6,7)
+
+#Nomear weekDays
+names(weekDays) <- var
+#apos a nomeação pode se chamar o vetor
+weekDays
+
+
+
+
