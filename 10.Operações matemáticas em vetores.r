c9c45eb63ea84ca8f7ad4aa8b71c94570529b23a
Henrique Assis <henrique_assis_10@hotmail.com>
2020-11-05 00:37:52 -0300

    operação vetores

---
+++ 10.Operações matemáticas em vetores.r
@@ -0,0 +1,15 @@
+#Vetor a ser calculado
+x <- c(1:10)
+x
+#soma
+x + 10
+#Mutiplicação
+x * 10
+#O valor de X continua o mesmo que foi declarado, não sofrendo nenhuma alteração
+#Se feita alguma operação matematica em um vetor, e quiser os dados alterados
+#se cria um novo vetor
+Xsoma <- x + 10
+Xsoma
+
+#Já na multiplicação entre vetores o comprimento tem que ser igual ou multiplo
+Xsoma * x
