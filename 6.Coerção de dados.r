3ce198fa14d05154adf917c3a2c1c993de04f685
Henrique Assis <henrique_assis_10@hotmail.com>
2020-11-04 23:08:04 -0300

    conversão

---
+++ 6.Coerção de dados.r
@@ -0,0 +1,11 @@
+#Conversão (Coersão) de dados
+#Passando como caractere
+var <- "1"
+var
+class(var)
+#Convertendo para Inteiro
+class(as.integer(var))
+
+#as.logical()
+#as.character()...
+#Lembrando que as conversões tem de ser condizente,para o dado que vai ser feita a conversão.
