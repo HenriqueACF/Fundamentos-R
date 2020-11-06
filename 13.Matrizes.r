2e066295a1b018164d7d00c329880fb824a80f54
Henrique Assis <henrique_assis_10@hotmail.com>
2020-11-05 23:24:12 -0300

    matriz

---
+++ 13.Matrizes.r
@@ -0,0 +1,12 @@
+#Criando matrizes
+# Vetores != Matrizes
+#Vetores são elementos em uma linha com varias colunas
+v <- c(1:10)
+v
+#Matrizes são varias linhas com variais colunas
+#matrix(data, [quantidade de linhas], [quantidade de colunas])
+matrix(c(1:100)) #1 coluna e 100 linhas
+matrix(c(1:100),  5) #5 linhas e 20 colunas
+#Atribuindo uma matriz ha uma variavel
+m <- matrix(c(1:100),  5)
+m
