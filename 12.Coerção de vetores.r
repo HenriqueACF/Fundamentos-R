fbcd3c1098b82a10304a2e9bb14437c3b7010ec4
Henrique Assis <henrique_assis_10@hotmail.com>
2020-11-05 23:06:22 -0300

    coerção de vetores

---
+++ 12.Coerção de vetores.r
@@ -0,0 +1,12 @@
+#Coerção de vetores
+numeros <- c(1:3)
+class(numeros)
+#atribuindo novos valores
+numeros <- c(numeros, 4.4)
+numeros
+class(numeros)
+#Vetor só pode ter um tipo de dado!
+#Adicionar uma string no vetor
+numeros <- c(numeros,"18")
+numeros
+class(numeros)
