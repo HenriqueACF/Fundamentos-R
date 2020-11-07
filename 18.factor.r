edea1125bdd0feb91e6f5f0a80d4123c07097bc9
Henrique Assis <henrique_assis_10@hotmail.com>
2020-11-07 17:50:56 -0300

    up

---
+++ 18.factor.r
@@ -0,0 +1,7 @@
+#Factors
+vetorJogadores <- c('Br', 'Br', 'Br', 'Br','Arg','Arg', 'Chi','Chi','Chi')
+vetorJogadores
+
+factorJogadores <-factor(vetorJogadores)
+#mostra os classificados
+factorJogadores
