75e9663099b8d9236e33b93ba02434b6928550f5
Henrique Assis <henrique_assis_10@hotmail.com>
2020-11-13 16:21:26 -0300

    if else

---
+++ 27.IF ElSE.r
@@ -0,0 +1,17 @@
+# IF e Else
+#Verificando se o numero na variavel é Par ou Impar
+numero <- 500
+
+if(numero %%2 == 0){
+    print("Par")
+}else{
+    print("Impar")
+}
+
+#exemplo 2
+teste <- "teste"
+if(class(teste) == "numeric"){
+    print("É numerico")
+}else{
+    print("Não é numerico")
+}
